88beb1436c63caf1150ff434e9208a3b3cceb45b
Alejandro <alejandro@example.com>
2026-05-08 16:18:02 -0500

    Fix: rutas base, favicon y build script

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://zazasmokeshop.github.io',
-  base: '/',
+  site: 'https://alejandro290513.github.io/zazasmokeshop.github.io/',
+  base: '/zazasmokeshop.github.io/',
 });
